5365008ec47a5dd4910f809e2930009c00b9cb7c
Tsukuro <vongola392112@gmail.com>
2024-04-03 00:59:23 +0700

    feat:done toast message and fetch api register when create user

--- front-end/src/pages/Register.tsx
+++ front-end/src/pages/Register.tsx
@@ -1,6 +1,10 @@
 import {useForm} from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
-type RegisterFormData={
+export type RegisterFormData={
     firstName:string,
     lastName:string,
     email:string,
@@ -9,14 +13,28 @@ type RegisterFormData={
 }
 
 const Register=()=>{
+    const {showToast}=useAppContext();
+    const navigate=useNavigate();
+
     const {
         register,
         watch, 
         handleSubmit,
         formState:{errors}
     }=useForm<RegisterFormData>();
+
+    const mutation=useMutation(apiClient.register,{
+        onSuccess:()=>{
+            showToast({message:"Register successful",type:"SUCCESS"});
+            navigate("/");
+        },
+        onError:(error:Error)=>{
+            showToast({message:error.message, type:"ERROR"});
+        }
+    });
+
     const onSubmit=handleSubmit((data)=>{
-        console.log(data);
+        mutation.mutate(data);
     })
 
     return (
