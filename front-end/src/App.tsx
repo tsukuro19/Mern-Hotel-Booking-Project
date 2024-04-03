3a368139489e7e5c7e2aee1f828c699557fb85a7
tsukuro19 <hoangthiennguyentran1204@gmail.com>
2024-04-03 19:45:57 +0700

    add module

---
+++ front-end/src/App.tsx
@@ -0,0 +1,42 @@
+import
+{
+  BrowserRouter as Router,
+  Route,
+  Routes, 
+  Navigate
+}
+from "react-router-dom"
+
+import Layout from "./layouts/Layouts"
+import Register from "./pages/Register"
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+          <Route path="/" element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+          <Route path="/search" element={
+              <Layout>
+                <p>Search page</p>
+              </Layout>
+            }
+          />
+        <Route path="/register" element={
+            <Layout>
+              <Register/>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
