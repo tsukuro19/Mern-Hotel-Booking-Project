499f58f195f3a6a3a9788b49099d416c083192b5
tsukuro19 <hoangthiennguyentran1204@gmail.com>
2024-04-03 20:20:55 +0700

    feat:all module

--- back-end/src/routes/auth.ts
+++ back-end/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import express,{Request,Response} from 'express';
 import { check, validationResult } from "express-validator";
 import User from "../models/user";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 
 const router=express.Router();
